40ba7995119d4680609775778696b3c4d5463bba
celinechole <github@celinechole.com>
2019-01-22 16:13:38 -0500

    Implements map API token

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('runs correctly');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2VsaW5lY2hvbGUiLCJhIjoiY2pyODkzZGVhMDRncjQ0cGRyeXVwYXFycyJ9.kgMgMQrH7X7bejHmPrOzJg';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
